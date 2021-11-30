e00510229f7661637f10bd137edcf2ccabfd3e46
AkshayBiju123 <75361973+AkshayBiju123@users.noreply.github.com>
2021-11-30 19:18:01 +1100

    Add files via upload

---
+++ UploadingImage.py
@@ -0,0 +1,48 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snapshot():
+    number = random.randint(0,100)
+    #initializing cv2
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        #read the frames while the camera is on
+        ret,frame = videoCaptureObject.read()
+        #cv2.imwrite() method is used to save an image to any storage device
+        img_name = "img"+str(number)+".png"
+        cv2.imwrite(img_name, frame)
+        start_time = time.time
+        result = False
+    return img_name
+    print("snapshot taken")
+    # releases the camera
+    videoCaptureObject.release()
+    #closes all the window that might be opened while this process
+    cv2.destroyAllWindows()
+
+
+
+def upload_file(img_name):
+    access_token = "sl.A9TkWpHkUj1SfOG9V1fYahhQnrBC1Zvpbi9DF2Kh1ivuIDmfsDRL3Kpp1oMYWt1h4mF4v5eGtM-mS5HMwyk968Tyy6CwIMBo9Zh4I7-S-uLT4j2cpCVewru1Jv8FfA1fUYBp2wxzMQ3g"
+    file =img_name
+    file_from = file
+    file_to="/testFolder/"+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode.overwrite)
+        print("file uploaded")
+
+
+def main():
+    while(True):
+        if ((time.time() - start_time) >= 5):
+            name = take_snapshot()
+            upload_file(name)
+
+main()
